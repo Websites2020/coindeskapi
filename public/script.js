076bce3c06f46ff791eebfe1855fb90545930eb4
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-26 00:12:46 -0400

    added Axios and bootstrap components

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        var d = new Date();
+    var d = new Date();
         var month = d.getMonth()+1;
         var year = d.getFullYear();
         var day = d.getDate()-1;
@@ -11,22 +10,31 @@ $(document).ready(function(){
             var date = year+"-"+month+"-"+day;
         }
         var oldDate = year+"-"+"0"+month+"-"+oldday;
+
+        $("#today").html(month + "/" + day + "/" + year);
+        $("#yesterday").html(month + "/" + oldday + "/" + year);
+        $("#yesterday2").html(month + "/" + oldday + "/" + year)
+
+    $("button").click(function(){
         
         console.log(date);
-        $.ajax({url: "https://api.coindesk.com/v1/bpi/historical/close.json", success: function(result){
-            var newData = JSON.parse(result);
+        $.ajax({url: "/coindesk", success: function(result){
+            
+            var newData = result;
+
             console.log(newData.bpi[date]);
             console.log(newData.bpi[oldDate]);
+
             if (newData.bpi[date] > newData.bpi[oldDate]) {
                 $("#price").addClass("up");
-                $("#price").html("$" + newData.bpi[date])
+                $("#price").html("$" + (newData.bpi[date]).toFixed(2))
                 $("#dolgainloss").addClass("up");
                 $("#dolgainloss").html("+$"+((newData.bpi[date]-newData.bpi[oldDate])).toFixed(2));
                 $("#pergainloss").addClass("up");
                 $("#pergainloss").html("+"+((newData.bpi[date]-newData.bpi[oldDate])/(newData.bpi[date])*100).toFixed(2)+"%");
             } else {
                 $("#price").addClass("down");
-                $("#price").html("$" + newData.bpi[date])
+                $("#price").html("$" + (newData.bpi[date]).toFixed(2))
                 $("#dolgainloss").addClass("down");
                 $("#dolgainloss").html("$"+((newData.bpi[date]-newData.bpi[oldDate])).toFixed(2));
                 $("#pergainloss").addClass("down");
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const axios = require('axios');
+
 
 app.use(express.static('public'))
+
+app.get('/coindesk', function (req, res) {
+  console.log("worked");
+
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+  .then(response => {
+    res.send(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+  })
 
 app.listen(port, () => console.log(`CoinDesk app listening on port ${port}!`))
 
